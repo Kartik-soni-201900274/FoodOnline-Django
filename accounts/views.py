2d30f0a96e2c57af936a9ddd004bd23bace87b51
kartik-soni-securitypal <kartik_201900274@smit.smu.edu.in>
2024-02-06 15:18:28 +0545

    view

--- accounts/views.py
+++ accounts/views.py
@@ -35,19 +35,29 @@ def registerVendor(request):
                 form = UserForm(request.POST)
                 v_form = VendorForm(request.POST, request.FILES)
                 if form.is_valid() and v_form.is_valid():
+
+                    #get user data
                     first_name = form.cleaned_data["first_name"]
                     last_name = form.cleaned_data["last_name"]
                     email = form.cleaned_data["email"]
                     username = form.cleaned_data["username"]
                     password = form.cleaned_data["password"]
+
+                    #create user and user profile
                     user = User.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username,password=password)
                     user.role = User.VENDOR
                     user.save()
                     user_profile = UserProfile.objects.get(user=user)
+                    
+                    #create vendor
                     vendor_name = v_form.cleaned_data['vendor_name']
                     vendor=Vendor(user=user, user_profile=user_profile, vendor_name=vendor_name, vendor_license=v_form.cleaned_data["vendor_license"],vendor_slug = slugify(vendor_name)+'-'+str(user.id))
-                    v_form = VendorForm(request.POST, instance=vendor)
-                    v_form.save()
+                    vendor.save()
+
+
+                    #also i can use below code the vendor is passed to the form as instance to save the vendor in database
+                    # v_form = VendorForm(request.POST, instance=vendor)
+                    # v_form.save()
     else:
         form = UserForm()
         v_form = VendorForm()
